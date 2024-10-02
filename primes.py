c2a53297369db3fc6e5e8b167e0e3f760af2caf0
Ziyu Zhang <ziyu.5.zhang@kcl.ac.uk>
2024-10-02 21:09:05 +0100

    update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError(f"number_of_primes={number_of_primes} should have been a positive number.")
+    def is_prime(n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+    count = 0
+    number = 2
+    while count < number_of_primes:
+        if is_prime(number):
+            list.append(number)
+            count += 1
+        number += 1
     return list
